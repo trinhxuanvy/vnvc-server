c0a642e6b7f9a40993388b237726c37b74bc3b04
nphungthinh2601 <nguyenphuchungthinhcongviec@gmail.com>
2022-07-04 22:26:04 +0700

    update vaccine search

--- controllers/VaccineController.js
+++ controllers/VaccineController.js
@@ -1,9 +1,66 @@
 const model = require('../models/Vaccine');
+const Category = require('../models/Category');
 
 class VaccineController {
   async simpleCreate(req, res, next) {
     try {
       const result = await model.create(req.body);
+      res.send(result);
+    } catch (error) {
+      res.send({
+        status: 500,
+        message: { err: error },
+      });
+    }
+  }
+  async getBySellType(req, res, next) {
+    try {
+      const hasType = !(
+        req.query?.type != 'Package' && req.query?.type != 'Retail'
+      );
+      const hasCategory =
+        req.query?.categoryId != '' && req.query?.categoryId != null;
+
+      const listCategory = (
+        await Category.find({
+          $or: [
+            { parentId: req.query?.categoryId },
+            { _id: req.query?.categoryId },
+          ],
+        })
+      ).map((x) => x._id);
+      const search = req.query?.search || '';
+      const result = hasType
+        ? hasCategory
+          ? await model
+              .find({
+                sellType: req.query?.type,
+                vaccineName: { $regex: '.*' + search + '.*' },
+              })
+              .where('categoryId')
+              .in(listCategory)
+          : await model.find({
+              sellType: req.query?.type,
+              vaccineName: { $regex: '.*' + search + '.*' },
+            })
+        : hasCategory
+        ? await model
+            .find({ vaccineName: { $regex: '.*' + search + '.*' } })
+            .where('categoryId')
+            .in(listCategory)
+        : await model.find({ vaccineName: { $regex: '.*' + search + '.*' } });
+
+      res.send({ total: result.length, entities: result });
+    } catch (error) {
+      res.send({
+        status: 500,
+        message: { err: error },
+      });
+    }
+  }
+  async getByById(req, res, next) {
+    try {
+      const result = await model.findById(req.params.id);
       res.send(result);
     } catch (error) {
       res.send({
--- routes/vaccine.js
+++ routes/vaccine.js
@@ -3,5 +3,7 @@ const router = express.Router();
 const VaccineController = require('../controllers/VaccineController');
 
 router.post('/vaccine', VaccineController.simpleCreate);
+router.get('/vaccine', VaccineController.getBySellType);
+router.get('/vaccine/:id', VaccineController.getByById);
 
 module.exports = router;
