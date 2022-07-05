e7e1cc84b86528c7da5f1a56734994a15d44c663
Ted Ung <tuanvan424@gmail.com>
2022-07-06 00:29:49 +0700

    update redis

--- controllers/VaccineController.js
+++ controllers/VaccineController.js
@@ -1,11 +1,14 @@
-fs = require("fs");
-const model = require("../models/Vaccine");
-const Category = require("../models/Category");
-const logger = require("../log/winston");
-const redis = require("redis");
+fs = require('fs');
+const model = require('../models/Vaccine');
+const Category = require('../models/Category');
+const logger = require('../log/winston');
+const Redis = require('redis');
 
-//const client = redis.createClient();
-//client.on("error", (err) => console.log("Redis Client Error", err));
+
+const resdisClient = Redis.createClient()
+
+
+const DEFAULT_EXPIRATION = 3600;
 
 class VaccineController {
   async simpleCreate(req, res, next) {
@@ -21,76 +24,77 @@ class VaccineController {
   }
   async getBySellType(req, res, next) {
     try {
-      const startTime = new Date();
-      logger.info(`Get Vaccine by filter - Start Time: ${startTime}`);
-      const hasType = !(
-        req.query?.type != "Package" && req.query?.type != "Retail"
-      );
-      const hasCategory =
-        req.query?.categoryId != "" && req.query?.categoryId != null;
-
-      const listCategory = (
-        await Category.find({
-          $or: [
-            { parentId: req.query?.categoryId },
-            { _id: req.query?.categoryId },
-          ],
-        })
-      ).map((x) => x._id);
-      const search = req.query?.search || "";
-      const result = hasType
-        ? hasCategory
-          ? await model
-              .find({
-                sellType: req.query?.type,
-                vaccineName: { $regex: ".*" + search + ".*" },
-              })
-              .where("categoryId")
-              .in(listCategory)
-          : await model.find({
-              sellType: req.query?.type,
-              vaccineName: { $regex: ".*" + search + ".*" },
-            })
-        : hasCategory
-        ? await model
-            .find({ vaccineName: { $regex: ".*" + search + ".*" } })
-            .where("categoryId")
-            .in(listCategory)
-        : await model.find({ vaccineName: { $regex: ".*" + search + ".*" } });
-
-      const endTime = new Date();
-      logger.info(`Get Vaccine by filter - End Time: ${endTime}`);
+      const type = req.query?.type;
+      const categoryId =  req.query?.categoryId;
+      await resdisClient.connect();
+      const startTimeRedis = new Date();
+      logger.info(`Get Vaccine by filter redis - Start Time: ${startTimeRedis}`);
+      let result =  await resdisClient.get(`vaccines?type=${type}?categoryId=${categoryId}`)
+      const endTimeRedis = new Date();
+      logger.info(`Get Vaccine by filter redis - End Time: ${endTimeRedis}`);
       logger.info(
-        `Get Vaccine by filter - Time Binding: ${
-          endTime.getTime() - startTime.getTime()
-        }ms`
+        `Get Vaccine by filter redis - Time Binding: ${
+          endTimeRedis.getTime() - startTimeRedis.getTime()
+        }ms`,
       );
-
-      // await client.connect();
-      // console.log("demo");
-      // await client.set("vaccines", "hello");
-      // console.log("demo");
-      // const startTimeRe = new Date();
-      // logger.info(`Get Vaccine by filter redis - Start Time: ${startTimeRe}`);
-      // client
-      //   .get("vaccines")
-      //   .then((data) => {
-      //     console.log(data);
-      //   })
-      //   .catch(async (err) => {
-      //     console.log(err);
-      //   });
-      // const endTimeRe = new Date();
-      // logger.info(`Get Vaccine by filter redis - End Time: ${endTimeRe}`);
-      // logger.info(
-      //   `Get Vaccine by filter redis - Time Binding: ${
-      //     endTimeRe.getTime() - startTimeRe.getTime()
-      //   }ms`
-      // );
-      //await client.disconnect();
-      res.send({ total: result.length, entities: result });
+      if (result) {
+        await resdisClient.disconnect();
+        const response = JSON.parse(result);
+        return res.send({total: response.length, entities: response})
+      }
+      else
+      {
+          const startTime = new Date();
+          logger.info(`Get Vaccine by filter - Start Time: ${startTime}`);
+          const hasType = !(
+            req.query?.type != 'Package' && req.query?.type != 'Retail'
+          );
+          const hasCategory =
+            req.query?.categoryId != '' && req.query?.categoryId != null;
+    
+          const listCategory = (
+            await Category.find({
+              $or: [
+                { parentId: req.query?.categoryId },
+                { _id: req.query?.categoryId },
+              ],
+            })
+          ).map((x) => x._id);
+          const search = req.query?.search || '';
+          const result = hasType
+            ? hasCategory
+              ? await model
+                  .find({
+                    sellType: req.query?.type,
+                    vaccineName: { $regex: '.*' + search + '.*' },
+                  })
+                  .where('categoryId')
+                  .in(listCategory)
+              : await model.find({
+                  sellType: req.query?.type,
+                  vaccineName: { $regex: '.*' + search + '.*' },
+                })
+            : hasCategory
+            ? await model
+                .find({ vaccineName: { $regex: '.*' + search + '.*' } })
+                .where('categoryId')
+                .in(listCategory)
+            : await model.find({ vaccineName: { $regex: '.*' + search + '.*' } });
+    
+          const endTime = new Date();
+          logger.info(`Get Vaccine by filter - End Time: ${endTime}`);
+          logger.info(
+            `Get Vaccine by filter - Time Binding: ${
+              endTime.getTime() - startTime.getTime()
+            }ms`,
+          );
+          await resdisClient.setEx(`vaccines?type=${type}?categoryId=${categoryId}`, DEFAULT_EXPIRATION, JSON.stringify(result));
+          await resdisClient.disconnect();
+          res.send({ total: result.length, entities: result });
+      }
     } catch (error) {
-      //await client.disconnect();
+      await resdisClient.disconnect();
+      console.log(error)
       res.send({
         status: 500,
         message: { err: error },
